52cf1013a7c9c9d25a953981f3f3e1af0368be37
michiya.torizumi <michiya.torizumi@gmail.com>
2022-07-26 11:14:13 +0900

    0726

--- app/src/views/organisms/Clients/NewClient/Office/PlanButton.tsx
+++ app/src/views/organisms/Clients/NewClient/Office/PlanButton.tsx
@@ -1,50 +1,26 @@
-import React, { useState } from 'react';
 
-import { Flex, PlanButton, PlanButtonInput, PlanButtonSubTitle, PlanButtonTitle, SForm, SPlanHead } from './Styled'
+import { Flex, PlanButton, PlanButtonSubTitle, PlanButtonTitle, SForm, SPlanHead } from './Styled'
 
 const OfficePlan = () => {
 
-  const [state, setState] = useState('Radio 1');
-
-  const handleChange = (e: { target: { value: React.SetStateAction<string>; }; }) => {
-    setState(e.target.value);
-    console.log(state);
-  }
+  const a = "無料プラン"
+  const b = "スタンダードプラン"
+  const c = "ビジネスプラン"
 
   return (
     <SForm>
       <SPlanHead>プランを選択</SPlanHead>
       <Flex>
-        {/* <PlanButton>
-          <PlanButtonTitle>無料プラン</PlanButtonTitle>
-          <PlanButtonSubTitle>XX ライセンスサポート</PlanButtonSubTitle>
-          <PlanButtonInput type="radio"
-            name='radioGroup'
-            id='radioGroup1'
-            value="Radio 1"
-            onChange={handleChange}
-            checked={state === 'Radio 1'}
-          />
-        </PlanButton>
-        <PlanButton>プラン２
-          <input type="radio"
-            name='radioGroup'
-            id='radioGroup2'
-            value="Radio 2"
-            onChange={handleChange}
-            checked={state === 'Radio 2'}
-          />
-        </PlanButton> */}
         <PlanButton>
-          <PlanButtonTitle>無料プラン</PlanButtonTitle>
+          <PlanButtonTitle>{a}</PlanButtonTitle>
           <PlanButtonSubTitle>XX ライセンスサポート</PlanButtonSubTitle>
         </PlanButton>
         <PlanButton>
-          <PlanButtonTitle>スタンダードプラン</PlanButtonTitle>
+          <PlanButtonTitle>{b}</PlanButtonTitle>
           <PlanButtonSubTitle>XX ライセンスサポート</PlanButtonSubTitle>
         </PlanButton>
         <PlanButton>
-          <PlanButtonTitle>ビジネスプラン</PlanButtonTitle>
+          <PlanButtonTitle>{c}</PlanButtonTitle>
           <PlanButtonSubTitle>XX ライセンスサポート</PlanButtonSubTitle>
         </PlanButton>
       </Flex>
